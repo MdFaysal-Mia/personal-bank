4c081f874a37a033ec1d689bb0772279a7115d05
MdFaysal-Mia <faysalmia7292@gmail.com>
2023-01-10 20:30:04 +0600

    login page js code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
-
+/* Login Button Event Handler */ 
+const loginButton = document.getElementById("login-btn");
+loginButton.addEventListener("click", function(){
+    const userEmail = document.getElementById("user-email").value;
+    const userPassword = document.getElementById("user-password").value;
+    // console.log("Email",userEmail,"Password",userPassword);
+    if(userEmail === "" && userPassword === "") {
+        alert("Please get your email and password");
+    }
+    else {
+        const loginSection = document.getElementById("login-section");
+        loginSection.style.display = "none";
+        const transactionSection = document.getElementById("transaction-section");
+        transactionSection.style.display = "block";
+    }
+});
 
